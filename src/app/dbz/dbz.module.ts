b676a3e584203c02e2557993c0167dc0046f6522
andgeral <geraldinebecerra93@gmail.com>
2024-07-09 17:00:00 -0400

    se crea un nuevo module y servicio de dbz

---
+++ src/app/dbz/dbz.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MainPageComponent } from './pages/main-page.component';
+import { ListComponent } from './components/list/list.component';
+import { AddCharacterComponent } from './components/addCharacter/addCharacter.component';
+
+@NgModule({
+  exports: [MainPageComponent],
+  declarations: [MainPageComponent, ListComponent, AddCharacterComponent],
+  imports: [CommonModule, FormsModule],
+})
+export class DbzModule {}
